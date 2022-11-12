a8275c0825d93fa633d79944afbda3d869aeb001
Ceci <admin@cecilias.me>
2022-11-12 09:22:40 -0600

    feat: setup foundation for database and api

--- src/pages/api/restricted.ts
+++ src/pages/api/restricted.ts
@@ -1,4 +1,4 @@
-import { unstable_getServerSession } from 'next-auth/next'
+import { unstable_getServerSession } from 'next-auth/next';
 import { authOptions } from './auth/[...nextauth]';
 
 export default async (req, res) => {
