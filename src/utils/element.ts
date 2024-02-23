d85af458908c04f87d2ad55cbf02ff4cbe79cf89
Cecilia Sanare <ceci@sanare.dev>
2024-02-22 20:05:25 -0600

    fix: prevent clicking from closing the popover on desktop

--- src/utils/element.ts
+++ src/utils/element.ts
@@ -1,15 +1,24 @@
 import { useCallback, useEffect, useState } from 'react';
 import { Actions } from '../constants/components';
+import { isMobile } from './mobile';
+
+export function isInPath(event: Event, element: HTMLElement) {
+    return element && event.composedPath().includes(element);
+}
+
+export function isNotInPath(event: Event, element: HTMLElement) {
+    return !isInPath(event, element);
+}
 
 export type EventProps = {
     [key: string]: Function;
 }
 
-export function useOpened(externallyOpened: boolean, event: Actions, closeOnClick: boolean): [boolean, EventProps] {
-    const [opened, setOpened] = useState(externallyOpened);
+export function useOpened<E extends HTMLElement>(options: Options<E>): [boolean, EventProps] {
+    const [opened, setOpened] = useState(options.opened);
 
     useEffect(() => {
-        if (opened && closeOnClick) {
+        if (opened && options.autoClose) {
             // Wait a frame before adding the click listener to prevent immediately closing
             requestAnimationFrame(() => {
                 document.addEventListener('click', close, {
@@ -23,20 +32,30 @@ export function useOpened(externallyOpened: boolean, event: Actions, closeOnClic
         return () => {
             document.removeEventListener('click', close);
         };
-    }, [opened, closeOnClick]);
+    }, [options.opened, options.autoClose]);
 
     const open = useCallback(() => setOpened(true), []);
-    const close = useCallback(() => setOpened(false), []);
+    const close = useCallback((event) => {
+        if (isNotInPath(event, options.ref.current)) {
+            setOpened(false);
+        }
+    }, []);
 
     let eventProps: EventProps = {};
 
-    if (event === Actions.CLICK) {
+    if (isMobile || options.action === Actions.CLICK) {
         eventProps.onClick = open;
-    } else if (event === Actions.HOVER) {
-        eventProps.onClick = open;
+    } else if (options.action === Actions.HOVER) {
         eventProps.onMouseEnter = open;
         eventProps.onMouseLeave = close;
     }
 
     return [opened, eventProps];
+}
+
+export type Options<E extends HTMLElement> = {
+    opened: boolean;
+    action: Actions;
+    autoClose: boolean;
+    ref: React.MutableRefObject<E>;
 }
